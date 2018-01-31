41dff810da6719c7031eb2ebf468fa8c2299d76e
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-01-31 13:19:27 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,38 @@
+#include<allegro5\allegro_primitives.h>
+
+using namespace std;
+
+int main()
+{
+	al_init();
+
+	al_init_primitives_addon();
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+	ALLEGRO_DISPLAY*gamewindow = NULL;
+	//ALLEGRO_DISPLAY*display = al_create_display(500, 500);
+
+	gamewindow = al_create_display(800, 800);
+	al_clear_to_color(al_map_rgb(128,0,0));
+	al_draw_filled_rectangle(500, 500, 250, 400, al_map_rgb(0, 128, 28));
+
+	al_draw_circle(250, 500, 50, al_map_rgb(50, 50, 50), 5);
+
+	al_draw_circle(500, 500, 50, al_map_rgb(50, 50, 50), 5);
+
+	al_flip_display();
+	
+		al_rest(100);
+
+		al_destroy_display(gamewindow);
+
+
+
+
+
+
+
+
+
+}
